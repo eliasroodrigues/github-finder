d06cae85cab043b642a1356c74752e3ac891183b
Elias <elias.roodrigues98@gmail.com>
2022-03-17 13:40:42 -0300

    fetch users data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import UserResults from '../components/users/UserResults'
+
 /*
  * Home Page
  *
@@ -6,9 +8,8 @@
 
 function Home() {
   return (
-    <div>
-      <h1 className='text-6xl'>Welcome</h1>
-    </div>
+    /* SEARCH COMPONENT GOES HERE */
+    <UserResults />
   )
 }
 
